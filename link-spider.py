41fc1160ea3b515ca6fb6bd20ad199ff976746e7
Mohamed Thalib H <hmthalib@gmail.com>
2016-11-04 11:09:40 +0530

    Updated script
    Signed-off-by: Mohamed Thalib H <hmthalib@gmail.com>

--- link-spider.py
+++ link-spider.py
@@ -17,14 +17,41 @@ from urllib.parse import urlparse
 ##Global variables
 master = []
 working = []
+webURL = 0
+verifySSL = True
+fd = 0
+##################
+###File operations
+def FileOpen(filename):
+	global fd
+	fd = open(filename, 'w')
 
+def FileClose():
+	global fd
+	fd.close()
+
+def FileWriteLine(line):
+	global fd
+	fd.write(line)
+	fd.write("\n")
+	fd.flush()
+
+def FileWriteList(links):
+	lfd = open('links_list.txt', 'w')
+	for line in links:
+		lfd.write(line)
+		lfd.write("\n")
+	lfd.close()
+	print ("Written the links to file links.txt")
+
+###################
 def getBaseURL(url):
 	temp = urlparse(url)
 	return temp.netloc
 
-def parse(base_url, url):
+def parse(base_url, url, verifySSL):
 	links = []
-	html = requests.get(url, verify=True).content
+	html = requests.get(url, verify=verifySSL).content
 	soup = BeautifulSoup(html, "html.parser")
 	for a in soup.find_all('a', href=True):
 		if ("http" in a['href']) & (base_url in a['href']):
@@ -45,22 +72,17 @@ def appendToWorking(links):
 	if(count != 0):
 		print("Found", count, "New URLS")
 
-def writeFile(links):
-	fd = open('links.txt', 'w')
-	for line in links:
-		fd.write(line)
-		fd.write("\n")
-	fd.close()
-	print ("Written the links to file links.txt")
 
 def help():
-	print (sys.argv[0] + ' -l <url>')
+	print (sys.argv[0] + ' <option> -l <url>')
+	print ('-n Disable SSL Verfiy')
 	sys.exit()
 
 ##Main script starts here
-webURL = 0
+
+
 try:
-	opts, args = getopt.getopt(sys.argv[1:],"hl:")
+	opts, args = getopt.getopt(sys.argv[1:],"nhl:")
 except getopt.GetoptError:
 	help()
 	sys.exit(2)
@@ -70,6 +92,8 @@ for opt, arg in opts:
 		help()
 	elif opt == '-l':
 		webURL = arg
+	elif opt == '-n':
+		verifySSL = False
 
 ##Prepare
 if(0 == webURL):
@@ -78,16 +102,17 @@ if(0 == webURL):
 baseURL = getBaseURL(webURL)
 print("BaseURL:", baseURL)
 working.append(webURL)
-
+FileOpen('links.txt')
 ##Find all
 count=1
 while (True):
 	for url in working:
 		if url not in master:
 			print("URL", len(master), '/', len(working), ":", url)
-			temp = parse(baseURL, url)
+			temp = parse(baseURL, url, verifySSL)
 			appendToWorking(temp)
 			master.append(url)
+			FileWriteLine(url)
 			count += 1
 	if(len(working) == len(master)):
 		break
@@ -97,4 +122,5 @@ while (True):
 list(set(master))
 master.sort()
 print ("\nTotal Links: ", len(master))
-writeFile(master)
+FileClose()
+FileWriteList(master)
